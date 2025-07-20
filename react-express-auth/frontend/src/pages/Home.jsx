69b8af6c3c924f8f966f3e33f334979676bee68f
OshaunB <oshaun1250@gmail.com>
2025-07-20 13:47:21 -0400

    finished hero section, its responsive, continuing landing

--- react-express-auth/frontend/src/pages/Home.jsx
+++ react-express-auth/frontend/src/pages/Home.jsx
@@ -1,8 +1,9 @@
-export default function HomePage() {
-  return <>
-    <h1>Home</h1>
-    <p>Put something interesting here!</p>
-    <p className="text-blue-500 text-2xl">Tailwind is working!</p>
+import HeroSection from "../components/Landing/HeroSection";
 
-  </>
+export default function HomePage() {
+  return (
+    <div>
+      <HeroSection />
+    </div>
+  );
 }
